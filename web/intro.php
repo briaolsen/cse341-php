3493f2a869e6457288826614e1b2f38ed9933f93
Briana Olsen <bria.olsen@gmail.com>
2020-04-30 19:35:52 -0600

    10

--- web/intro.php
+++ web/intro.php
@@ -9,8 +9,7 @@
   <link href='https://fonts.googleapis.com/css?family=Satisfy' rel='stylesheet'>
 
   <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-    integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
   <title>CSE 341 Briana Olsen Introduction</title>
 </head>
@@ -20,8 +19,7 @@
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 
     <a class="navbar-brand" href="intro.html">Briana Olsen</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
 
@@ -55,56 +53,56 @@
   <div class="container">
     <div class="card-deck">
 
-      
-        <div class="card" style="width: 20rem;">
-          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/1280px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg" class="card-img-top" alt="Starry Night, June 1889">
-          <div class="card-body d-flex flex-column">
-            <h5 class="card-title">Starry Night</h5>
-            <h6 class="card-subtitle mb-2 text-muted">June 1889</h6>
-            <p class="card-text">Painted during his stay at the Saint-Paul-de-Mausole lunatic asylum of the view from his window, Starry Night is one of the most recognized paintings in the history of Western culture.</p>
-            <a href="https://en.wikipedia.org/wiki/The_Starry_Night" class="btn btn-secondary mt-auto">Learn More</a>
-          </div>
-        </div>
-      
 
-      
-        <div class="card" style="width: 20rem;">
-          <div id="portraitCarousel" class="carousel slide" data-ride="carousel">
-            <div class="carousel-inner">
-              <div class="carousel-item active">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/Vincent_van_Gogh_-_Self-portrait_with_pipe_-_Google_Art_Project.jpg/800px-Vincent_van_Gogh_-_Self-portrait_with_pipe_-_Google_Art_Project.jpg" class="d-block w-100" alt="Self Portrait with Pipe, 1886">
-            </div>
-              <div class="carousel-item">
-                  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Van_Gogh_Self-Portrait_with_Straw_Hat_1887-Detroit.jpg/800px-Van_Gogh_Self-Portrait_with_Straw_Hat_1887-Detroit.jpg" class="d-block w-100" alt="Self Portrait with Straw Hat, Summer 1887">
-              </div>
-              <div class="carousel-item">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/VanGogh-self-portrait-with_bandaged_ear.jpg/800px-VanGogh-self-portrait-with_bandaged_ear.jpg" class="d-block w-100" alt="Self-Portrait with Bandaged Ear, Easel, and Japanese Print, January 1889">
-              </div>
-              <div class="carousel-item">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Vincent_van_Gogh_-_Self-Portrait_-_Google_Art_Project.jpg/800px-Vincent_van_Gogh_-_Self-Portrait_-_Google_Art_Project.jpg" class="d-block w-100" alt="Self Portrait, September 1889">
-              </div>
-            </div>
-          </div>
-          <div class="card-body d-flex flex-column">
-            <h5 class="card-title">Self Portraits</h5>
-            <h6 class="card-subtitle mb-2 text-muted">1853-1890</h6>
-            <p class="card-text">Van Gogh painted dozens of self portraits in various styles.</p>
-            <a href="https://en.wikipedia.org/wiki/Portraits_of_Vincent_van_Gogh" class="btn btn-secondary mt-auto">Learn More</a>
-          </div>
+      <div class="card" style="width: 20rem;">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/1280px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg" class="card-img-top" alt="Starry Night, June 1889">
+        <div class="card-body d-flex flex-column">
+          <h5 class="card-title">Starry Night</h5>
+          <h6 class="card-subtitle mb-2 text-muted">June 1889</h6>
+          <p class="card-text">Painted during his stay at the Saint-Paul-de-Mausole lunatic asylum of the view from his window, Starry Night is one of the most recognized paintings in the history of Western culture.</p>
+          <a href="https://en.wikipedia.org/wiki/The_Starry_Night" class="btn btn-secondary mt-auto">Learn More</a>
         </div>
-      
+      </div>
 
-      
-        <div class="card" style="width: 20rem;">
-          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Irises-Vincent_van_Gogh.jpg/1280px-Irises-Vincent_van_Gogh.jpg" class="card-img-top" alt="Irises, May 1889">
-          <div class="card-body d-flex flex-column">
-            <h5 class="card-title">Irises</h5>
-            <h6 class="card-subtitle mb-2 text-muted">May 1889</h6>
-            <p class="card-text">Van Gogh painted several pictures of irises during his stay at Saint Paul-de-Mausole asylum where he was inspired by the hospital garden.</p>
-            <a href="https://en.wikipedia.org/wiki/The_Starry_Night" class="btn btn-secondary mt-auto">Learn More</a>
+
+
+      <div class="card" style="width: 20rem;">
+        <div id="portraitCarousel" class="carousel slide" data-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/Vincent_van_Gogh_-_Self-portrait_with_pipe_-_Google_Art_Project.jpg/800px-Vincent_van_Gogh_-_Self-portrait_with_pipe_-_Google_Art_Project.jpg" class="d-block w-100" alt="Self Portrait with Pipe, 1886">
+            </div>
+            <div class="carousel-item">
+              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Van_Gogh_Self-Portrait_with_Straw_Hat_1887-Detroit.jpg/800px-Van_Gogh_Self-Portrait_with_Straw_Hat_1887-Detroit.jpg" class="d-block w-100" alt="Self Portrait with Straw Hat, Summer 1887">
+            </div>
+            <div class="carousel-item">
+              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/VanGogh-self-portrait-with_bandaged_ear.jpg/800px-VanGogh-self-portrait-with_bandaged_ear.jpg" class="d-block w-100" alt="Self-Portrait with Bandaged Ear, Easel, and Japanese Print, January 1889">
+            </div>
+            <div class="carousel-item">
+              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Vincent_van_Gogh_-_Self-Portrait_-_Google_Art_Project.jpg/800px-Vincent_van_Gogh_-_Self-Portrait_-_Google_Art_Project.jpg" class="d-block w-100" alt="Self Portrait, September 1889">
+            </div>
           </div>
         </div>
-     
+        <div class="card-body d-flex flex-column">
+          <h5 class="card-title">Self Portraits</h5>
+          <h6 class="card-subtitle mb-2 text-muted">1853-1890</h6>
+          <p class="card-text">Van Gogh painted dozens of self portraits in various styles.</p>
+          <a href="https://en.wikipedia.org/wiki/Portraits_of_Vincent_van_Gogh" class="btn btn-secondary mt-auto">Learn More</a>
+        </div>
+      </div>
+
+
+
+      <div class="card" style="width: 20rem;">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Irises-Vincent_van_Gogh.jpg/1280px-Irises-Vincent_van_Gogh.jpg" class="card-img-top" alt="Irises, May 1889">
+        <div class="card-body d-flex flex-column">
+          <h5 class="card-title">Irises</h5>
+          <h6 class="card-subtitle mb-2 text-muted">May 1889</h6>
+          <p class="card-text">Van Gogh painted several pictures of irises during his stay at Saint Paul-de-Mausole asylum where he was inspired by the hospital garden.</p>
+          <a href="https://en.wikipedia.org/wiki/The_Starry_Night" class="btn btn-secondary mt-auto">Learn More</a>
+        </div>
+      </div>
+
     </div>
   </div>
 
@@ -120,26 +118,19 @@
   <div>
     <h3>How old would Van Gogh be?</h3>
     <?php
-    $today = getdate();
-    $vanGogh = date_create("1853-03-30");
-    $diff = date_diff($vanGogh, $today);
-
+    $date1 = date_create("2013-03-15");
+    $date2 = date_create("2013-12-12");
+    $diff = date_diff($date1, $date2);
     echo $diff->format("%R%a days");
-  ?>
+    ?>
   </div>
-  
+
 
 
   <!-- jQuery, Popper.js, Bootstrap JS -->
-  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
-    integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
-    crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-    integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
-    crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
-    integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
-    crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </body>
 
 </html>
