e4b0829b36d9b571d9ff59ab0ef87c0d16e4308d
Briana Olsen <bria.olsen@gmail.com>
2020-05-21 15:50:21 -0600

    styling photo10

--- web/Library/search.php
+++ web/Library/search.php
@@ -34,7 +34,7 @@ $genres = array("realistic fiction","historical fiction",  "fantasy", "science f
 
     <h1>Search for Book</h1>
 
-    <div class="row">
+    <div class="row" id="search-form">
     <form class="col-6">
 
       <div class="form-row">
