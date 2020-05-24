92382019ab56f3e43524c15da2820482e5fa4c2d
Briana Olsen <bria.olsen@gmail.com>
2020-05-23 23:35:26 -0600

    Library finished search

--- web/Library/database.php
+++ web/Library/database.php
@@ -16,7 +16,8 @@ function get_db() {
     $dbPassword = $dbOpts["pass"];
     $dbName = ltrim($dbOpts["path"],'/');
 
-    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+    //$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+    $db = new PDO('postgres://herymnpumbdvjl:5fb0816b5f1b1f359576c0e56a0a5ee887564e76e8c14105b88d98c11d52ad07@ec2-52-201-55-4.compute-1.amazonaws.com:5432/d57aqo8h5p3d3i');
 
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   }
