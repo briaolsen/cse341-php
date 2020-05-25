4836a5fb70432a693504975b65c8bd28aa0dd9f4
Briana Olsen <bria.olsen@gmail.com>
2020-05-25 16:17:21 -0600

    reverted database.php

--- web/Library/database.php
+++ web/Library/database.php
@@ -16,8 +16,7 @@ function get_db() {
     $dbPassword = $dbOpts["pass"];
     $dbName = ltrim($dbOpts["path"],'/');
 
-    //$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-    $db = new PDO('postgres://herymnpumbdvjl:5fb0816b5f1b1f359576c0e56a0a5ee887564e76e8c14105b88d98c11d52ad07@ec2-52-201-55-4.compute-1.amazonaws.com:5432/d57aqo8h5p3d3i');
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   }
@@ -27,5 +26,5 @@ function get_db() {
     die();
   }
 
-  return $db;
+return $db;
 }
