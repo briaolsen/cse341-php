2dcea20f80ea9702263f64bcaea4c772c8442ff9
Briana Olsen Laptop <bria.olsen@gmail.com>
2020-06-03 17:29:43 -0600

    Add Assignments to navigation

--- web/Library/navbar.php
+++ web/Library/navbar.php
@@ -8,8 +8,8 @@
     
       <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
         <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-          <li class="nav-item <?php if($currentPage == "home") echo "active";?>">
-            <a class="nav-link" href="index.php">Home</a>
+          <li class="nav-item">
+            <a class="nav-link" href="../assignments.html">Assignments</a>
           </li>
           <li class="nav-item <?php if($currentPage == "library") echo "active";?>">
             <a class="nav-link" href="library.php">Library</a>
